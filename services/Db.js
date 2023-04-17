5ef84614df2c62a66dae6d5bf4136bb2c412338a
Bisal424 <122355300+Bisal424@users.noreply.github.com>
2023-04-17 12:30:39 +0530

    changes in mogo uri

--- services/Db.js
+++ services/Db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('Dotenv').config();
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = mongodb+srv://<user>:<user>@cluster0.nrmaauw.mongodb.net/Inventory-Management?retryWrites=true&w=majority;
 
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
